fd8f88345f942584c46f444dfed04ad29de68568
ldotmithu <ldotmithurshan222@gmail.com>
2025-07-17 17:03:50 +0530

    updated foldure structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="doctor appoinments sysytem",
+    version="0.0.0",
+    author="ldotmithurshan",
+    author_email="ldotmithurshan222@gmail.com",
+    packages=find_packages()
+)
