0ce54ea4d9a2a28fd38f8ea6513996ed5a434c64
ldotmithu <ldotmithurshan222@gmail.com>
2026-04-13 19:27:06 +0530

    update model

--- utils/config.py
+++ utils/config.py
@@ -8,7 +8,7 @@ OPENAI_API_KEY = os.getenv("GROQ_API_KEY")
 LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
 LANGSMITH_PROJECT = os.getenv("LANGSMITH_PROJECT")
 
-LLM_MODEL = "qwen/qwen3-32b"
+LLM_MODEL = "openai/gpt-oss-120b"
 TEMPERATURE = 0.9
 
 BACKEND_URL = ""
